9a0fe9d4f923092c70471f1f34e7dc798ed46736
RobertHJones <roberthuwjones@outlook.com>
2022-02-07 21:02:16 +0000

    database

--- db/index.js
+++ db/index.js
@@ -1,16 +1,24 @@
 // const { Pool } = require('pg')
 import pg from "pg";
-import { host, dbname, username, password, port } from "../config.js";
+import {
+  host,
+  dbname,
+  username,
+  password,
+  port,
+  databaseURL,
+} from "../config.js";
 
 console.log(host);
 // const pool = new Pool()
 const pool = new pg.Pool({
   // data from heroku
-  user: username,
-  host: host,
-  database: dbname,
-  password: password,
-  port: port,
+  // user: username,
+  // host: host,
+  // database: dbname,
+  // password: password,
+  // port: port,
+  connectionString: databaseURL,
   ssl: { rejectUnauthorized: false },
 });
 
