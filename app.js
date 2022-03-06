1b38daefb10e7946f6bd5a5b721cd3cb53890ffb
RobertHJones <roberthuwjones@outlook.com>
2022-03-06 12:08:59 +0000

    removed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from "cors";
+// import cors from "cors";
 
 import indexRouter from "./routes/index.js";
 // import usersRouter from "./routes/users.js";
@@ -11,7 +11,7 @@ import indexRouter from "./routes/index.js";
 var app = express();
 
 app.use(logger("dev"));
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
