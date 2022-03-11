016bb1a129d26ea64fe78cf9876511dbb278ed96
RobertHJones <roberthuwjones@outlook.com>
2022-03-11 13:33:51 +0000

    changing

--- app.js
+++ app.js
@@ -43,15 +43,6 @@ import indexRouter from "./routes/index.js";
 
 // export default app;
 
-import createError from "http-errors";
-import express from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-
-import indexRouter from "./routes/index.js";
-// import usersRouter from "./routes/users.js";
-
 var app = express();
 
 app.use(logger("dev"));
