1c0dc8ac88cb4557f63abe53686a26a55812957c
RobertHJones <roberthuwjones@outlook.com>
2022-03-11 13:32:59 +0000

    changegg

--- app.js
+++ app.js
@@ -8,14 +8,53 @@ import logger from "morgan";
 import indexRouter from "./routes/index.js";
 // import usersRouter from "./routes/users.js";
 
-const app = express();
+// const app = express();
+
+// app.use(logger("dev"));
+// app.use((req, res, next) => {
+//   res.header({ "Access-Control-Allow-Origin": "*" });
+//   next();
+// });
+// // app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static("public"));
+
+// app.use("/", indexRouter);
+// // app.use("/users", usersRouter);
+// app.set("view engine", "jade");
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
+// export default app;
+
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+// import usersRouter from "./routes/users.js";
+
+var app = express();
 
 app.use(logger("dev"));
-app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*" });
-  next();
-});
-// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
