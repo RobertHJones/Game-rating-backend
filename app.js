c501e24774b4ac435fe46ed37b47b35483c55727
RobertHJones <roberthuwjones@outlook.com>
2022-03-15 18:13:03 +0000

    dang

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ import indexRouter from "./routes/index.js";
 const app = express();
 
 app.use(logger("dev"));
-app.use((req, res, next) => {
-  res.header({ "Access-Control-Allow-Origin": "*" });
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header({ "Access-Control-Allow-Origin": "*" });
+//   next();
+// });
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: false }));
