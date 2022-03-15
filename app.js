01d064397c22ff6d12eb7df372485609e18c87b0
RobertHJones <roberthuwjones@outlook.com>
2022-03-15 18:19:17 +0000

    image change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import cors from "cors";
+// import cors from "cors";
 
 import indexRouter from "./routes/index.js";
 // import usersRouter from "./routes/users.js";
@@ -11,11 +11,11 @@ import indexRouter from "./routes/index.js";
 const app = express();
 
 app.use(logger("dev"));
-// app.use((req, res, next) => {
-//   res.header({ "Access-Control-Allow-Origin": "*" });
-//   next();
-// });
-app.use(cors());
+app.use((req, res, next) => {
+  res.header({ "Access-Control-Allow-Origin": "*" });
+  next();
+});
+// app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
