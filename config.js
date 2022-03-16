00c35c5a27b47d99ba1fb1dda0d9b3e315088dfe
RobertHJones <roberthuwjones@outlook.com>
2022-03-16 09:38:15 +0000

    image

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export const password = process.env.PGPASSWORD;
 export const port = process.env.PGPORT;
 export const databaseURL = process.env.DATABASE_URL;
 
-// export const cloudName = process.env.CLOUD_NAME;
-// export const apiKey = process.env.API_KEY;
-// export const apiSecret = process.env.API_SECRET;
+export const cloudName = process.env.CLOUD_NAME;
+export const apiKey = process.env.API_KEY;
+export const apiSecret = process.env.API_SECRET;
